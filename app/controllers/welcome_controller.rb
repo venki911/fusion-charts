597a7b5874a1b217f6153283847246df5570f473
dht <dht@ubuntu.(none)>
2012-11-30 21:43:28 +0800

    fc

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+#encoding: utf-8
+
+class WelcomeController < ApplicationController
+
+# render_sm_chart(chart_id, chart_type, category, datasets, options = {}, chart_width = 700, chart_height = 500, show_total = false)
+	def index 
+		
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FusionCharts::Application.config.secret_token = '7604dab085d565df90e79198e61a81640740a3605d82235be62ff29e8689c559b7f13c9b5cbb029a520f11870a6244c747b8ebafd15f84c1be3aa9983149efdd'
